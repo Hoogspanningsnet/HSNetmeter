349a516f99b0ce0d4617fa191079dfa66a0c4a2f
BaDu-HSN <bvanduijnhoven@solcon.nl>
2018-03-29 22:15:23 +0200

    Silent mode toegevoegd
    Zodat de logging de rpi niet te traag maakt.

--- Software RPI/hsnetmeter.py
+++ Software RPI/hsnetmeter.py
@@ -22,6 +22,7 @@ emptypayload = {'clID': ClientID, 'meas':[]}
 maxpayloadlength = 10
 payload = emptypayload
 expert = 1
+silent = 1
 
 #define callbackfunction for counting the sines
 def countingcallback(gpio, level, tick):
@@ -29,49 +30,55 @@ def countingcallback(gpio, level, tick):
     global sinecount
     global payload
     if sinecount == 0:
-        firstuptick = tick
+       firstuptick = tick
     sinecount += 1
     if sinecount == 8:
-        pi.write(LED_PIN, 0)
+       pi.write(LED_PIN, 0)
     if sinecount > 100:
-        freq = 50000000/(tick-firstuptick)
-        if expert == 1:
-            print (datetime.datetime.utcnow().strftime('%H:%M:%S:%f') + " - Frequentie: {0:.4f} Hz".format(freq),end="")
-        sinecount = 1
-        firstuptick = tick
-        pi.write(LED_PIN, 1)
-        payload['meas'].append({'freq': round((freq*10000)-(def_freq*10000)),'volt': round((volt*10)-(def_volt*10)),'utc':round(time.time(),3)})
-        top10 = copy.deepcopy(payload)
-        if len(top10['meas']) >= maxpayloadlength:
-            top10['meas'] = top10['meas'][:maxpayloadlength]
-        try:
-            r = requests.post(url+str(tick), data=json.dumps(top10), timeout=0.15)
-            if r.status_code != 200:
-                r.raise_for_status()
-        except requests.exceptions.HTTPError as errh:
-            print (" - ERROR: HTTP error - Resultbuffer: ", len(payload['meas'])," ", str(errh).split(' ', 1)[0])
-        except requests.exceptions.ConnectionError:
-            print (" - ERROR: URL not found - Resultbuffer: ", len(payload['meas']))
-        except requests.exceptions.Timeout:
-            print (" - ERROR: Timeout - Resultbuffer: ", len(payload['meas']))
-        except requests.exceptions.RequestException as err:
-            print (" - ERROR: General error - Resultbuffer: ", len(payload['meas'])," ", err)
-        else:
-            if r.text.find('SUCCES') > -1:
-                payload['meas'] = payload['meas'][maxpayloadlength:]
-            if expert == 1:
+       freq = 50000000/(tick-firstuptick)
+       if silent != 1:
+          if expert == 1:
+             print (datetime.datetime.utcnow().strftime('%H:%M:%S:%f') + " - Frequentie: {0:.4f} Hz".format(freq),end="")
+       sinecount = 1
+       firstuptick = tick
+       pi.write(LED_PIN, 1)
+       payload['meas'].append({'freq': round((freq*10000)-(def_freq*10000)),'volt': round((volt*10)-(def_volt*10)),'utc':round(time.time(),3)})
+       top10 = copy.deepcopy(payload)
+       if len(top10['meas']) >= maxpayloadlength:
+          top10['meas'] = top10['meas'][:maxpayloadlength]
+       try:
+          r = requests.post(url+str(tick), data=json.dumps(top10), timeout=0.15)
+          if r.status_code != 200:
+             r.raise_for_status()
+       except requests.exceptions.HTTPError as errh:
+          if silent != 1:
+             print (" - ERROR: HTTP error - Resultbuffer: ", len(payload['meas'])," ", str(errh).split(' ', 1)[0])
+       except requests.exceptions.ConnectionError:
+          if silent != 1:
+             print (" - ERROR: URL not found - Resultbuffer: ", len(payload['meas']))
+       except requests.exceptions.Timeout:
+          if silent != 1:
+             print (" - ERROR: Timeout - Resultbuffer: ", len(payload['meas']))
+       except requests.exceptions.RequestException as err:
+          if silent != 1:
+             print (" - ERROR: General error - Resultbuffer: ", len(payload['meas'])," ", err)
+       else:
+          if r.text.find('SUCCES') > -1:
+             payload['meas'] = payload['meas'][maxpayloadlength:]
+          if silent != 1:
+             if expert == 1:
                 print(" -", r.text, "- Resultbuffer: ", len(payload['meas']))
-            
+
 #start the mesurements
 try:
-    print("Starting frequency measurements, press ctrl-c to quit.\n")
-    pi = pigpio.pi()
-    pi.set_mode(LED_PIN, pigpio.OUTPUT)
-    cb = pi.callback(HONDERDHZ_PIN, pigpio.RISING_EDGE, countingcallback)
-    while True:
-        time.sleep(0.1)
+   print("Starting frequency measurements, press ctrl-c to quit.\n")
+   pi = pigpio.pi()
+   pi.set_mode(LED_PIN, pigpio.OUTPUT)
+   cb = pi.callback(HONDERDHZ_PIN, pigpio.RISING_EDGE, countingcallback)
+   while True:
+      time.sleep(0.1)
 except KeyboardInterrupt:
-    print ("\nFrequency measurement stopped through ctrl-c.")
-    cb.cancel()
-    pi.write(LED_PIN, 0)
-    pi.stop()
+   print ("\nFrequency measurement stopped through ctrl-c.")
+   cb.cancel()
+   pi.write(LED_PIN, 0)
+   pi.stop()
